7a60fc5d9e52aa60b7fb4992fec29cebf29eedb7
Sebastiaandb <info@sebastiaandebruin.com>
2024-05-08 15:49:15 +0200

    first commit for testproject

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Just to test git',
+    author='Sebastiaan de Bruin',
+    license='MIT',
+)
